036ff8030d41cab098f6f58e9766e65dc497add9
Abubakar98-github <abubkarmansha100@gmail.com>
2023-12-07 04:28:43 +0500

    changing

--- index.js
+++ index.js
@@ -1,82 +1,55 @@
-let cards = document.querySelectorAll(".card");
-let cartNum = document.getElementById("cartNum")
-let submitBtn = document.getElementById("submitBtn")
-let cartIcon = document.getElementById("cartIcon");
+ let card = document.querySelectorAll(".card");
+ let submit = document.getElementById("submit");
+ let num = 0;
 
+ document.querySelector(".cartItems").style.display="none"
 
-let num = 0;
+ card.forEach(function(cards){
+    cards.addEventListener("click", function(){
+        console.log(cards);
 
-cartIcon.addEventListener("click", function(){
-    console.log("ssssss");
-    document.querySelector(".cartItems").style.display="none";
-
-})
-
-let cartItem = document.querySelector(".cartItems");
-cartItem.style.display="none";
-
-cards.forEach(function(card){
-
-    card.addEventListener("click", function(){
-        console.log(card);
         document.querySelector(".cartItems").style.display="block"
+
+        num++;
+        document.querySelector("#cartNum").innerHTML=num;
         let div = document.createElement("div");
         div.classList.add("cartList");
         div.innerHTML=`
         <i class="fa-solid fa-xmark" onclick="remove()"></i>
-        <img src=${card.firstElementChild.src}>
-        <p>$199<p/>
+        <img src=${cards.firstElementChild.src}>
+        <p>$199</p>
         `
-       cartItem.appendChild(div)
-
-        num++;
-        cartNum.innerHTML=num;
-
-
-     
+        document.querySelector(".cartItems").appendChild(div)
     })
-})
- 
+ })
+
  function remove(){
     num--;
     let cartList = document.querySelectorAll(".cartList");
-console.log(cartList);
-cartList.forEach(function(currentItems){
-    currentItems.addEventListener("click", function(){
-        
-        if(num == 0){
-            cartItem.style.display="none"
-        }
-            currentItems.style.display="none"
-             
-            cartNum.innerHTML=num;
+    cartList.forEach(function(currentItem){
+        currentItem.addEventListener("click", function(){
+            if(num == 0){
+                document.querySelector(".cartItems").style.display="none"
+            }
+            currentItem.style.display= "none"
+        document.querySelector("#cartNum").innerHTML=num;
 
-        
-         
-        // console.log(currentItems.style.display="none");
+        })
     })
-    console.log(currentItems);
-
-})
  }
- 
 
 
 
 
-
-
-
-
-
- submitBtn.addEventListener("click", function(){
+//  contact
+submit.addEventListener("click", function(){
     let email = document.getElementById("email");
     let pass = document.getElementById("pass");
 
     if(email.value == "" && pass.value == ""){
-        alert("Please Enter Email and Password");
+        alert("Enter Email and Password")
     }else{
-        alert("Thnaks You Logged In");
+        alert("Thanks You Logged IN")
     }
 
- })
+})
